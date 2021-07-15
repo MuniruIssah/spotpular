978b8632d9496da4548a0c25d9d5e1920a69733a
MuniruIssah <imuniru@st.ug.edu.gh>
2021-07-15 08:26:12 +0000

    Created Landing Page for Spotpular

---
+++ src/pages/LandingPage/landingPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import HeaderAndContentLayout from "../../layouts/headerAndContent/headerAndContentLayout";
+const LandingPage = () => {
+  return <HeaderAndContentLayout></HeaderAndContentLayout>;
+};
+
+export default LandingPage;
