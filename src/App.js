f5385ca7ce682effe92ab540eb1a29c782b51fa4
MuniruIssah <imuniru@st.ug.edu.gh>
2021-07-15 08:20:36 +0000

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
-import HeaderAndContentLayout from "./layouts/headerAndContent/headerAndContentLayout";
 
 function App() {
   return (
     <div className="App">
-      <HeaderAndContentLayout />
+      <Router>
+        <Route/>
+        <Route/>
+      </Router>
     </div>
   );
 }
